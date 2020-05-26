9cfac3bb061d9ddff0cced26bc7dd68d67c6cbf8
amitlzkpa <amit.lzkpa@gmail.com>
2020-05-26 10:23:24 -0500

    try https on router and not app

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,21 @@ const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID;
 
 
 
+//HTTPS redirect middleware
+function ensureSecure(req, res, next) {
+  //Heroku stores the origin protocol in a header variable. The app itself is isolated within the dyno and all request objects have an HTTP protocol.
+  if (req.get('X-Forwarded-Proto') == 'https' || req.hostname == 'localhost') {
+    //Serve Vue App by passing control to the next middleware
+    next();
+  } else if (req.get('X-Forwarded-Proto') != 'https' && req.get('X-Forwarded-Port') != '443') {
+    //Redirect if not HTTP with original request URL
+    res.redirect('https://' + req.hostname + req.url);
+  }
+}
+//attach middleware to app
+router.all('*', ensureSecure);
+
+
 // these aren't really secrets
 router.get('/api/auth0-secrets', function(req, res) {
   const creds = {
