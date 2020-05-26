0d16abc287c726f607905727d72eaa272b1c1b9f
amitlzkpa <amit.lzkpa@gmail.com>
2020-05-26 10:59:53 -0500

    try securing through app.use

--- server/server.js
+++ server/server.js
@@ -17,6 +17,25 @@ mongoose.connect(mongoURI, {
 });
 
 const app = express();
+
+
+
+//HTTPS redirect middleware
+function ensureSecure(req, res, next) {
+  //Heroku stores the origin protocol in a header variable. The app itself is isolated within the dyno and all request objects have an HTTP protocol.
+  if (req.get('X-Forwarded-Proto') == 'https' || req.hostname == 'localhost') {
+    //Serve Vue App by passing control to the next middleware
+    next();
+  } else if (req.get('X-Forwarded-Proto') != 'https' && req.get('X-Forwarded-Port') != '443') {
+    //Redirect if not HTTP with original request URL
+    res.redirect('https://' + req.hostname + req.url);
+  }
+}
+//attach middleware to app
+app.use('*', ensureSecure);
+
+
+
 app.use(express.static(DIR));
 
 app.use(cookieParser());
