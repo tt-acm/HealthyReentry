113e0ab6a36c3da2b2b41390db4f5ce7992e03e7
amitlzkpa <amit.lzkpa@gmail.com>
2020-06-23 14:11:29 -0500

    start setting up modal to download office stats

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -127,6 +127,31 @@
       </div>
     </div>
 
+    <!-- Office Stats Download Modal -->
+    <div class="modal fade" id="downloadOfficeStatsModal" tabindex="-1" role="dialog" aria-labelledby="downloadOfficeStatsLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="downloadOfficeStatsLabel">Download Stats for Offices</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div>
+              <h6>
+                Download Office Stats
+              </h6>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-light" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal" @click="downloadOfficeStats">Download</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <h5 class="text-muted">Admin Dashboard</h5>
 
     <hr class="my-3"/>
@@ -310,7 +335,7 @@
             <span class="dropdown-item" @click="downloadSelectedAsCSV">
               Download Data
             </span>
-            <span class="dropdown-item" @click="downloadOfficeStats">
+            <span class="dropdown-item" data-toggle="modal" data-target="#downloadOfficeStatsModal">
               Download Office Stats
             </span>
           </div>
@@ -592,8 +617,7 @@ export default {
       downloadCSV(csv, `encounters_${new Date().toLocaleDateString()}:${new Date().getHours()}:${new Date().getMinutes()}.csv`);
     },
     downloadOfficeStats() {
-      let csv = "Office,Orange,Red,Total Signups\r\n";
-      downloadCSV(csv, `office-stats_${new Date().toLocaleDateString()}:${new Date().getHours()}:${new Date().getMinutes()}.csv`);
+      console.log('foo');
     },
     async refreshData(ignoreOfcFilters) {
 
