896a2417d63b8ed1549ea4498dd550fb4cef991f
hanshenSun <hanshensunw@gmail.com>
2021-05-18 17:59:59 -0400

    change vaccine manufacturer order

--- src/partials/VaccinationDropdown.vue
+++ src/partials/VaccinationDropdown.vue
@@ -3,15 +3,15 @@
     <md-field class="mb-3">
         <label for="font">Choose the manufacturer</label>
         <md-select v-model="curVaccineManufacturer" name="vaccine" id="vaccine" placeholder="Choose the manufacturer" :md-selected="$emit('vaccineSelected', curVaccineManufacturer, curIndex)">
-            <md-option value="Pfizer">Pfizer-BioNTech</md-option>
-            <md-option value="Moderna">Moderna</md-option>
-            <md-option value="JohnsonJohnson">Johnson & Johnson’s Janssen</md-option>
             <md-option value="AstraZeneca">AstraZeneca</md-option>
-            <md-option value="Novavax">Novavax</md-option>
-            <md-option value="Covishield">Covishield</md-option>       
-            <md-option value="Sinopharm">Sinopharm</md-option>
+            <md-option value="JohnsonJohnson">Johnson & Johnson’s Janssen</md-option>
+            <md-option value="Moderna">Moderna</md-option>
+            <md-option value="Pfizer">Pfizer-BioNTech</md-option>
             <md-option value="CanSinoBIO">CanSinoBIO</md-option>
-            <md-option value="CVnCoV">CVnCoV</md-option>                      
+            <md-option value="Covishield">Covishield</md-option> 
+            <md-option value="CVnCoV">CVnCoV</md-option>               
+            <md-option value="Novavax">Novavax</md-option>                  
+            <md-option value="Sinopharm">Sinopharm</md-option>   
             <md-option value="Other">Other</md-option>
         </md-select>
     </md-field>
