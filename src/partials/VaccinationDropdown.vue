d784193eefdafdd7e6e69a28cc144b00bcdcef9c
hanshenSun <hanshensunw@gmail.com>
2021-05-25 17:38:26 -0400

    Update Vaccination options

--- src/partials/VaccinationDropdown.vue
+++ src/partials/VaccinationDropdown.vue
@@ -3,15 +3,18 @@
     <md-field class="mb-3">
         <label for="font">Choose the manufacturer</label>
         <md-select v-model="curVaccineManufacturer" name="vaccine" id="vaccine" placeholder="Choose the manufacturer" :md-selected="$emit('vaccineSelected', curVaccineManufacturer, curIndex)">
-            <md-option value="AstraZeneca">AstraZeneca</md-option>
-            <md-option value="JohnsonJohnson">Johnson & Johnson’s Janssen</md-option>
+            <md-option value="AstraZeneca">Oxford-AstraZeneca</md-option>
+            <md-option value="JohnsonJohnson">Johnson & Johnson</md-option>
             <md-option value="Moderna">Moderna</md-option>
             <md-option value="Pfizer">Pfizer-BioNTech</md-option>
+            <md-option value="Bharat Biotech">Bharat Biotech</md-option>
             <md-option value="CanSinoBIO">CanSinoBIO</md-option>
             <md-option value="Covishield">Covishield</md-option> 
             <md-option value="CVnCoV">CVnCoV</md-option>               
-            <md-option value="Novavax">Novavax</md-option>                  
-            <md-option value="Sinopharm">Sinopharm</md-option>   
+            <md-option value="Novavax">Novavax</md-option>  
+            <md-option value="Sinopharm">Sinopharm</md-option>                 
+            <md-option value="Sinovac">Sinovac</md-option>   
+            <md-option value="Sputnik V">Sputnik V</md-option>
             <md-option value="Other">Other</md-option>
         </md-select>
     </md-field>
